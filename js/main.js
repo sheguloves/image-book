ff5ee637f431d0100be597d9d321c06efa93bba1
Felix Xi <felix.xi@sap.com>
2015-12-09 10:17:17 +0800

    Add main.js to implement image book by using jquery and make reactjs run well.

--- js/main.js
+++ js/main.js
@@ -1,71 +1,105 @@
 $(function() {
+    var urls = ["images/001.jpg", "images/002.jpg", "images/003.jpg", "images/004.jpg",
+                "images/005.jpg", "images/006.jpg", "images/007.jpg", "images/008.jpg"];
     var $mybook = $('#mybook');
     var $bttn_next = $('#next_page_button');
     var $bttn_prev = $('#prev_page_button');
     var $loading = $('#loading');
-    var $mybook_images = $mybook.find('img');
-    var cnt_images = $mybook_images.length;
+    var $no_image = $('.no_image');
+
+    var useObjectUrl = false;
+    var imagelist = [];
+
+    var length = 0;
     var loaded = 0;
-    //preload all the images in the book,
-    //and then call the booklet plugin
 
-    $mybook_images.each(function() {
-        var $img = $(this);
-        var source = $img.attr('src');
-        $('<img/>').load(function() {
-            ++loaded;
-            if (loaded == cnt_images) {
-                $loading.hide();
-                $bttn_next.show();
-                $bttn_prev.show();
-                $mybook.show().booklet({
-                    name: null, // name of the booklet to display in the document title bar
-                    width: 800, // container width
-                    height: 500, // container height
-                    speed: 600, // speed of the transition between pages
-                    direction: 'LTR', // direction of the overall content organization, default LTR, left to right, can be RTL for languages which read right to left
-                    startingPage: 0, // index of the first page to be displayed
-                    easing: 'easeInOutQuad', // easing method for complete transition
-                    easeIn: 'easeInQuad', // easing method for first half of transition
-                    easeOut: 'easeOutQuad', // easing method for second half of transition
+    var loadHandler = function() {
+        loaded++;
+        if (loaded === length) {
+            addBooklet();
+            loaded = 0;
+        }
+    };
 
-                    closed: true, // start with the book "closed", will add empty pages to beginning and end of book
-                    closedFrontTitle: null, // used with "closed", "menu" and "pageSelector", determines title of blank starting page
-                    closedFrontChapter: null, // used with "closed", "menu" and "chapterSelector", determines chapter name of blank starting page
-                    closedBackTitle: null, // used with "closed", "menu" and "pageSelector", determines chapter name of blank ending page
-                    closedBackChapter: null, // used with "closed", "menu" and "chapterSelector", determines chapter name of blank ending page
-                    covers: false, // used with  "closed", makes first and last pages into covers, without page numbers (if enabled)
+    function revokeUrls(urls) {
+        if (useObjectUrl) {
+            urls.forEach(function(item) {
+                window.URL.revokeObjectURL(item);
+            });
+            useObjectUrl = false;
+        }
+    }
 
-                    pagePadding: 10, // padding for each page wrapper
-                    pageNumbers: true, // display page numbers on each page
+    var renderImages = function(imagelist) {
+        if (imagelist.length > 0) {
+            length = imagelist.length;
+            $no_image.hide();
+            imagelist.forEach(function(item) {
+                var $img = $('<img>');
+                $img.one('load', loadHandler);
+                $img.attr('src', item);
+                $mybook.append($img);
+            });
+            $mybook.children('img').each(function() {
+                if (this.complete) {
+                    $(this).load();
+                }
+            });
+        } else {
+            $no_image.show();
+        }
+    }
 
-                    hovers: false, // enables preview pageturn hover animation, shows a small preview of previous or next page on hover
-                    overlays: false, // enables navigation using a page sized overlay, when enabled links inside the content will not be clickable
-                    tabs: false, // adds tabs along the top of the pages
-                    tabWidth: 60, // set the width of the tabs
-                    tabHeight: 20, // set the height of the tabs
-                    arrows: false, // adds arrows overlayed over the book edges
-                    cursor: 'pointer', // cursor css setting for side bar areas
+    var addBooklet = function() {
+        $loading.hide();
+        $bttn_next.show();
+        $bttn_prev.show();
+        $mybook.show().booklet({
+            name: null, // name of the booklet to display in the document title bar
+            width: 800, // container width
+            height: 500, // container height
+            speed: 600, // speed of the transition between pages
+            direction: 'LTR', // direction of the overall content organization, default LTR, left to right, can be RTL for languages which read right to left
+            startingPage: 0, // index of the first page to be displayed
+            easing: 'easeInOutQuad', // easing method for complete transition
+            easeIn: 'easeInQuad', // easing method for first half of transition
+            easeOut: 'easeOutQuad', // easing method for second half of transition
 
-                    hash: false, // enables navigation using a hash string, ex: #/page/1 for page 1, will affect all booklets with 'hash' enabled
-                    keyboard: true, // enables navigation with arrow keys (left: previous, right: next)
-                    next: $bttn_next, // selector for element to use as click trigger for next page
-                    prev: $bttn_prev, // selector for element to use as click trigger for previous page
+            pagePadding: 10, // padding for each page wrapper
+            pageNumbers: true, // display page numbers on each page
 
-                    menu: null, // selector for element to use as the menu area, required for 'pageSelector'
-                    pageSelector: false, // enables navigation with a dropdown menu of pages, requires 'menu'
-                    chapterSelector: false, // enables navigation with a dropdown menu of chapters, determined by the "rel" attribute, requires 'menu'
+            next: $bttn_next, // selector for element to use as click trigger for next page
+            prev: $bttn_prev, // selector for element to use as click trigger for previous page
+        });
+    }
 
-                    shadows: true, // display shadows on page animations
-                    shadowTopFwdWidth: 166, // shadow width for top forward anim
-                    shadowTopBackWidth: 166, // shadow width for top back anim
-                    shadowBtmWidth: 50, // shadow width for bottom shadow
+    var removeBooklet = function() {
+        $bttn_next.hide();
+        $bttn_prev.hide();
+        $mybook.hide().booklet("destroy");
+        $mybook.empty();
+    }
 
-                    before: function() {}, // callback invoked before each page turn animation
-                    after: function() {} // callback invoked after each page turn animation
-                });
+    var $fileDialog = $('#fileDialog');
+    $fileDialog.change(function(event) {
+        $loading.show();
+        revokeUrls(imagelist);
+        removeBooklet();
+        var files = event.target.files;
+        imagelist = [];
+        if (files.length > 0) {
+            useObjectUrl = true;
+            var imagelist = [];
+            var src;
+            for (var i = 0; i < files.length; i++) {
+                src = window.URL.createObjectURL(files[i]);
+                imagelist.push(src);
             }
-        }).attr('src', source);
+        }
+
+        renderImages(imagelist);
     });
+
+    renderImages(urls);
 
 });
---
+++ js/src/booker.js
@@ -0,0 +1,138 @@
+var urls = ["images/001.jpg", "images/002.jpg", "images/003.jpg", "images/004.jpg",
+            "images/005.jpg", "images/006.jpg", "images/007.jpg", "images/008.jpg"];
+
+var Book = React.createClass({
+    removeBooklet: function() {
+        var $mybook = $('#mybook');
+        $mybook.booklet("destroy");
+    },
+    addBooklet: function() {
+        var $loading = $('#loading');
+        var $bttn_next = $('#next_page_button');
+        var $bttn_prev = $('#prev_page_button');
+        var $mybook = $('#mybook');
+        $loading.hide();
+        $bttn_next.show();
+        $bttn_prev.show();
+        $mybook.show().booklet({
+            width: 800, // container width
+            height: 500, // container height
+            speed: 300, // speed of the transition between pages
+            startingPage: 0, // index of the first page to be displayed
+
+            closed: true,   // start with the book "closed", will add empty pages to beginning and end of book
+
+            pagePadding: 10, // padding for each page wrapper
+            pageNumbers: true, // display page numbers on each page
+
+            keyboard: true, // enables navigation with arrow keys (left: previous, right: next)
+            next: $bttn_next, // selector for element to use as click trigger for next page
+            prev: $bttn_prev, // selector for element to use as click trigger for previous page
+
+            shadows: false, // display shadows on page animations
+        });
+    },
+    componentWillUnmount: function() {
+        // remove previous booklet before new images loading
+        this.removeBooklet();
+        var $loading = $('#loading');
+        var $bttn_next = $('#next_page_button');
+        var $bttn_prev = $('#prev_page_button');
+        var $mybook = $('#mybook');
+        $loading.show();
+        $bttn_next.hide();
+        $bttn_prev.hide();
+        $mybook.hide();
+    },
+    componentDidUpdate: function() {
+        // add booklet immediately after new images loading
+        this.addBooklet();
+    },
+    componentDidMount: function() {
+        // add booklet immediately after initial images loading
+        this.addBooklet();
+    },
+    render: function() {
+        var temp;
+        var images = this.props.images
+        if (images && images.length > 0) {
+            temp = [];
+            images.forEach(function(item) {
+                temp.push(<ImageItem key={item} imagesrc={item} />);
+            });
+        } else {
+            temp = <div> No image selected </div>
+        }
+        return (
+            <div className="book_wrapper">
+                <a id="next_page_button"></a>
+                <a id="prev_page_button"></a>
+                <div id="mybook" style={{display: 'none'}}>
+                    {temp}
+                </div>
+            </div>
+        );
+    }
+});
+
+var ImageItem = React.createClass({
+    loadHandler: function() {
+        window.URL.revokeObjectURL(this.src);
+    },
+    render: function() {
+        return (
+            <div className="image_box">
+                <img src={this.props.imagesrc} onLoad={this.loadHandler}/>
+            </div>
+        );
+    }
+});
+
+var Files = React.createClass({
+    handleFiles: function(event) {
+        var files = event.target.files;
+        if (files.length === 0) {
+            return null;
+        } else {
+            var imagelist = [];
+            var src;
+            for (var i = 0; i < files.length; i++) {
+                src = window.URL.createObjectURL(files[i]);
+                imagelist.push(src);
+            }
+            this.props.onImageLoaded(imagelist);
+        }
+    },
+    render: function() {
+        return (
+            <div>
+                <input id="fileDialog" type="file" multiple onChange={this.handleFiles} accept="image/*" style={{display:'none'}}/>
+                <label style={{cursor: 'pointer'}} htmlFor="fileDialog">Select Files</label>
+            </div>
+        )
+    }
+});
+
+var Booker = React.createClass({
+    getInitialState: function() {
+        return {
+            images: urls
+        };
+    },
+    handleImageLoaded: function(imageslist) {
+        this.setState({images: imageslist});
+    },
+    render: function() {
+        return (
+            <div>
+                <Book images={this.state.images} />
+                <Files onImageLoaded={this.handleImageLoaded} />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Booker />,
+    document.getElementById('booker')
+);
